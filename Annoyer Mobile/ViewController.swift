efa6a0ed9f3d7b21d921cc766042b8cdeffe0758
tyler58546 <tyler@kk2.com>
2017-09-02 09:33:54 -0700

    -

--- Annoyer Mobile/ViewController.swift
+++ Annoyer Mobile/ViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toneSelector: UISegmentedControl!
+    @IBOutlet weak var voiceSelector: UISegmentedControl!
+    @IBOutlet weak var ttsField: UITextField!
+    
+    var tonePlayer: AVAudioPlayer!
+    
+    @IBAction func sayButtonPressed(_ sender: Any) {
+        
+        let voiceid = voiceSelector.selectedSegmentIndex
+        var voicelang = "en-US"
+        
+        
+        
+        if (voiceid == 0) {voicelang = "en-GB"}
+        if (voiceid == 1) {voicelang = "en-IE"}
+        if (voiceid == 2) {voicelang = "en-US"}
+        if (voiceid == 3) {voicelang = "en-ZA"}
+        
+        let string = ttsField.text
+        let utterance = AVSpeechUtterance(string: string!)
+        utterance.voice = AVSpeechSynthesisVoice(language: voicelang)
+        
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
+    @IBAction func tonePlayButtonPressed(_ sender: Any) {
+        let toneid = toneSelector.selectedSegmentIndex
+        var file = "1000hz.wav"
+        
+        if (toneid != 4) {
+            
+            
+            if (toneid == 0) {file = "1000hz.wav"}
+            if (toneid == 1) {file = "5000hz.wav"}
+            if (toneid == 2) {file = "10000hz.wav"}
+            if (toneid == 3) {file = "15000hz.wav"}
+            
+            let path = Bundle.main.path(forResource: file, ofType:nil)!
+            let url = URL(fileURLWithPath: path)
+        
+            do {
+                let sound = try AVAudioPlayer(contentsOf: url)
+                tonePlayer = sound
+                sound.play()
+            } catch {
+                // couldn't load file :(
+            }
+        }
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
